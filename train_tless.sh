37a68758a4c6ecde2a9c80792ea7aca6a0688981
Peter Hoenig <hpfreak1004@gmail.com>
2024-02-14 16:52:45 +0100

    update

---
+++ train_tless.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {2..4}
+do
+    config_file="configs/Template-LBBDM-f4_tless_${i}.yaml"
+    python3 main.py --config "$config_file" --train --sample_at_start --save_top --gpu_ids 0
+done
